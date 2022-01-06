a2d9cd652540f0d47a6517825b79a78cf49afab9
Wouter Scherphof <wouter.scherphof@gmail.com>
2022-01-06 17:25:00 +0100

    material/button.pug complete

--- html/main.js
+++ html/main.js
@@ -1,5 +1,6 @@
 import './style.scss'
 import 'htmx.org' // htmx from or { htmx } from don't work
+import 'https://unpkg.com/hyperscript.org@0.9.3' // not yet on npm
 import { MDCRipple } from '@material/ripple'
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -7,15 +8,23 @@ document.addEventListener('DOMContentLoaded', () => {
     .forEach((button) => new MDCRipple(button))
 })
 
-// standard(no-undef)
-htmx.on('htmx:configRequest', function (evt) { // eslint-disable-line
-  if (window.location.host.startsWith('localhost') && evt.detail.path.startsWith('/')) {
+htmx.on('htmx:configRequest', function ({ detail }) { // eslint-disable-line
+  if (window.location.host.startsWith('localhost') && detail.path.startsWith('/')) {
     // vite dev server proxy to fastify
-    evt.detail.path = '/api' + evt.detail.path
+    detail.path = '/api' + detail.path
   }
 })
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+htmx.on('htmx:load', function ({ detail }) { // eslint-disable-line
+  const { elt } = detail
+  const buttonClass = 'mdc-button'
+  if (elt.classList.contains(buttonClass)) {
+    ripple(elt)
+  }
+  elt.querySelectorAll('.' + buttonClass).forEach((button) => {
+    ripple(button)
+  })
+  function ripple (button) {
+    return new MDCRipple(button)
+  }
+})
--- routes/root.js
+++ routes/root.js
@@ -2,9 +2,6 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return { root: true }
-  })
-  fastify.get('/clicked', async function (request, reply) {
-    return reply.view('clicked')
+    return reply.view('app')
   })
 }
