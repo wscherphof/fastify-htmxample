4f6a2cc0ba7f0ea917938eac4bb694143cdedabf
Wouter Scherphof <wouter.scherphof@gmail.com>
2022-01-07 12:20:34 +0100

    material/iconButton

--- html/main.js
+++ html/main.js
@@ -2,11 +2,7 @@ import './style.scss'
 import 'htmx.org' // htmx from or { htmx } from don't work
 import 'https://unpkg.com/hyperscript.org@0.9.3' // not yet on npm
 import { MDCRipple } from '@material/ripple'
-
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('.mdc-button')
-    .forEach((button) => new MDCRipple(button))
-})
+import { MDCIconButtonToggle } from '@material/icon-button'
 
 htmx.on('htmx:configRequest', function ({ detail }) { // eslint-disable-line
   if (window.location.host.startsWith('localhost') && detail.path.startsWith('/')) {
@@ -15,16 +11,30 @@ htmx.on('htmx:configRequest', function ({ detail }) { // eslint-disable-line
   }
 })
 
-htmx.on('htmx:load', function ({ detail }) { // eslint-disable-line
-  const { elt } = detail
-  const buttonClass = 'mdc-button'
-  if (elt.classList.contains(buttonClass)) {
-    ripple(elt)
-  }
-  elt.querySelectorAll('.' + buttonClass).forEach((button) => {
-    ripple(button)
+const material = {}
+material.initButton = (element) => {
+  ['mdc-button', 'mdc-icon-button'].forEach((buttonClass) => {
+    if (element.classList.contains(buttonClass)) {
+      init(element)
+    }
+    element.querySelectorAll('.' + buttonClass).forEach((button) => {
+      init(button)
+    })
+    function init (button) {
+      const ripple = new MDCRipple(button)
+      if (buttonClass === 'mdc-icon-button') {
+        ripple.unbounded = true
+        if (button.getAttribute('toggle')) {
+          const iconToggle = new MDCIconButtonToggle(button)
+          iconToggle.listen('MDCIconButtonToggle:change', (event) => {
+            button.dispatchEvent(event)
+          })
+        }
+      }
+    }
   })
-  function ripple (button) {
-    return new MDCRipple(button)
-  }
+}
+
+htmx.on('htmx:load', function ({ detail }) { // eslint-disable-line
+  material.initButton(detail.elt)
 })
