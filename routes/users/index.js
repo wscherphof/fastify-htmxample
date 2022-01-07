337099c29c184a63f0f4313f7f5578cf2268221e
Wouter Scherphof <wouter.scherphof@gmail.com>
2022-01-07 20:24:12 +0100

    cosmetic

--- routes/users/index.js
+++ routes/users/index.js
@@ -8,19 +8,25 @@ module.exports = async function (fastify, opts) {
   fastify.post('/', async function (request, reply) {
     const { email, password, password2 } = request.body
     if (password !== password2) {
-      throw fastify.httpErrors.badRequest("passwords don't match")
+      badRequest("passwords don't match")
+    }
+    if (!password.length) {
+      badRequest("no password")
     }
     const hash = await fastify.bcrypt.hash('password')
-    const users = this.mongo.db.collection('users')
+    const users = fastify.mongo.db.collection('users')
     try {
       await users.insertOne({ email, password: hash })
       return 'ok'
     } catch (error) {
       if (error.code === 11000) {
-        throw fastify.httpErrors.badRequest('email already taken')
+        badRequest('email already taken')
       } else {
         throw error
       }
+    }
+    function badRequest(message) {
+      throw fastify.httpErrors.badRequest(message)
     }
   })
 }
