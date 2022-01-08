ba92f0d6a8725d089eefe2690c33dea70a37e02f
Wouter Scherphof <wouter.scherphof@gmail.com>
2022-01-08 11:26:22 +0100

    wip material/textField

--- html/main.js
+++ html/main.js
@@ -3,6 +3,7 @@ import 'htmx.org' // htmx from or { htmx } from don't work
 import 'https://unpkg.com/hyperscript.org@0.9.3' // not yet on npm
 import { MDCRipple } from '@material/ripple'
 import { MDCIconButtonToggle } from '@material/icon-button'
+import { MDCTextField } from '@material/textfield';
 
 htmx.on('htmx:configRequest', function ({ detail }) { // eslint-disable-line
   if (detail.path === '/') { // since <div id="app" hx-get="/" hx-trigger="load"></div>
@@ -19,25 +20,31 @@ htmx.on('htmx:configRequest', function ({ detail }) { // eslint-disable-line
 })
 
 htmx.on('htmx:load', function ({ detail }) { // eslint-disable-line
-  const { elt } = detail;
-  ['mdc-button', 'mdc-icon-button'].forEach((buttonClass) => {
-    if (elt.classList.contains(buttonClass)) {
-      init(elt)
-    }
-    elt.querySelectorAll('.' + buttonClass).forEach((button) => {
-      init(button)
+  function init(classes, init) {
+    classes = classes instanceof Array ? classes : [classes]
+    const { elt } = detail;
+    classes.forEach((className) => {
+      if (elt.classList.contains(className)) {
+        init(elt, className)
+      }
+      elt.querySelectorAll('.' + className).forEach((elt) => {
+        init(elt, className)
+      })
     })
-    function init(button) {
-      const ripple = new MDCRipple(button)
-      if (buttonClass === 'mdc-icon-button') {
-        ripple.unbounded = true
-        if (button.getAttribute('toggle')) {
-          const iconToggle = new MDCIconButtonToggle(button)
-          iconToggle.listen('MDCIconButtonToggle:change', (event) => {
-            button.dispatchEvent(event)
-          })
-        }
+  }
+  init(['mdc-button', 'mdc-icon-button'], (elt, className) => {
+    const ripple = new MDCRipple(elt)
+    if (className === 'mdc-icon-button') {
+      ripple.unbounded = true
+      if (elt.getAttribute('toggle')) {
+        const iconToggle = new MDCIconButtonToggle(elt)
+        iconToggle.listen('MDCIconButtonToggle:change', (event) => {
+          elt.dispatchEvent(event)
+        })
       }
     }
+  })
+  init('mdc-text-field', (elt, className) => {
+    return new MDCTextField(elt)
   })
 })
