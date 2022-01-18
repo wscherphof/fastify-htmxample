02ab77546d8eb6870c4870670254ce7558b279f5
Wouter Scherphof <wouter.scherphof@gmail.com>
2022-01-18 20:31:29 +0100

    bump fastify-cookie-auth, fastify-htmx, dev-htmx

--- routes/root.js
+++ routes/root.js
@@ -1,12 +1,9 @@
 'use strict'
 
 module.exports = async function (fastify, opts) {
-  async function app(request, reply, auth) {
-    const user = await auth
-    return reply.view('app', { user, request })
-  }
-  fastify.auth.optional.get('/', app)
-  fastify.auth.optional.get('/app', app)
+  fastify.get('/', async function app(request, reply) {
+    return reply.view('app', { request })
+  })
 
   fastify.auth.get('/secret', async function (request, reply) {
     return reply.view('secret', { request })
